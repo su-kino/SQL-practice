21cf88c94ff1d49cc3afb1ad6eeeeb996181d3ed
su-kino <97873142+su-kino@users.noreply.github.com>
2022-05-14 13:33:24 +0900

    [practice]4

---
+++ 4/4-1/2.sql
@@ -0,0 +1,7 @@
+-- studentsテーブルのidカラムの値が6のレコードをnameをJordanに、courseをHTMLに変更してください。
+UPDATE students
+SET name = "Jordan", course = "HTML"
+WHERE id = 6;
+
+-- 下記のクエリは消さないでください。
+SELECT * FROM students WHERE id=6;
---
+++ 4/4-1/3.sql
@@ -0,0 +1,6 @@
+-- studentsテーブルからidカラムの値が7のレコードを削除してください。
+DELETE FROM students
+WHERE id = 7;
+
+-- 下記のクエリは消さないでください。
+SELECT * FROM students;
